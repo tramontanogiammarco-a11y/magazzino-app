ce2923f3b7e253ae8c0167f31b0cf04c072bc92a
Giammarco Tramontano <galluse@Mac.homenet.telecomitalia.it>
2026-04-30 16:35:26 +0200

    Point Sheets sync to updated webhook

--- src/constants/sheetsWebhook.js
+++ src/constants/sheetsWebhook.js
@@ -1,3 +1,3 @@
 /** URL Web App Google Apps Script (sincronizzazione foglio). */
 export const SHEETS_WEBHOOK_URL =
-  'https://script.google.com/macros/s/AKfycbzyFfqEVnAUx4cPR5ch4YhBzoYVtTV3ys4xnq3laU0Ea06tSV5wfetxRWu31d4lduEpLg/exec'
+  'https://script.google.com/macros/s/AKfycbzFpFs3GRaw945xLb2SqHM1VopzOCHY7-mOoE1OGnqMPV86ll2EqJS6U6SXnXO5QcI23w/exec'
